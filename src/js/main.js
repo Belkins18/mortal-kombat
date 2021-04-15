02730e8fbd6b61564056d9c5c35b5ce9354cb871
Nikolay Belibov <nikolay.belibov@gmail.com>
2021-04-15 23:26:36 +0300

    finish tasks

--- src/js/main.js
+++ src/js/main.js
@@ -50,13 +50,14 @@ const ATTACK = ['head', 'body', 'foot'];
 // Dom
 const $root: HTMLDivElement = document.querySelector('.root');
 const $arena: HTMLDivElement = document.querySelector('.arenas');
-const $randomBtn: HTMLButtonElement = document.querySelector('.button');
 const $switchMode: HTMLInputElement = document.getElementById('checkbox');
 const $formFight: HTMLFontElement = document.querySelector('.control');
 
 // Global State
 let players: [] = [];
 let playersDOM: [] = [];
+let myPlayer;
+let enemyPlayer;
 
 const createEl = (tag: string, className: string | null = null) => {
     if (!className) return document.createElement(tag);
@@ -83,20 +84,19 @@ const randomInteger = (min, max) => {
 
 // Class
 class CreatePlayer {
-    constructor(name: string, hp: number, img: string, weapon: [string], number: number) {
+    constructor(name: string, hp: number, img: string, weapon: [string], number: number, isEnemy: Boolean) {
         this.name = name;
         this.hp = hp;
         this.img = img;
         // this.weapon = weapon
         this.playerNumber = `player${number}`
+        this.isEnemy = isEnemy;
     }
 
-    // attack() {
-    //     console.log(`${this.name} Fight...`)
-    // }
-    changeHP() {
-        this.hp -= randomInteger(1, 20);
+    changeHP(value) {
+        this.hp -= value;
     }
+
     renderHP() {
         const $playerLife = this.elHP();
 
@@ -106,6 +106,7 @@ class CreatePlayer {
             $playerLife.style.width = `${this.hp}%`;
         }
     }
+
     elHP() {
         return document.querySelector(`.${this.playerNumber} .life`)
     }
@@ -126,13 +127,15 @@ const createPlayer = (player: {
     name: string,
     hp: number,
     img: string,
-    weapon: [string]
+    weapon: [string],
+    isEnemy: Boolean
 }) => {
-    const { name, hp, img, playerNumber } = player
+    const { name, hp, img, playerNumber, isEnemy } = player
     const $root = createEl('div', playerNumber);
     const $progressbar = createEl('div', 'progressbar');
     const $life = createEl('div', 'life');
     const $name = createEl('div', 'name');
+    const $isPlayer = createEl('div', 'isPlayer');
     const $character = createEl('div', 'character');
     const $img = createEl('img');
 
@@ -142,6 +145,10 @@ const createPlayer = (player: {
 
     $progressbar.appendChild($life);
     $progressbar.appendChild($name);
+    if (!isEnemy) {
+        $progressbar.appendChild($isPlayer);
+    }
+    
     $character.appendChild($img);
 
     $root.appendChild($progressbar);
@@ -156,7 +163,8 @@ if (localStorage.getItem('gameMod')) {
 const getRandomPlayer = (list) => {
     return list[Math.floor(Math.random() * list.length)]
 }
-const generatePlayers = (hardMode: boolean = false, _heroes = heroes): void => {
+
+const generatePlayers = (hardMode: boolean = false, myPlayer: number = 0, _heroes = heroes): void => {
     Array(2).fill(0).forEach((item, index) => {
         const key = getRandomPlayer(Object.keys(_heroes))
         const hero = heroes[key];
@@ -167,10 +175,12 @@ const generatePlayers = (hardMode: boolean = false, _heroes = heroes): void => {
             hardMode ? randomHP : hero.hp,
             hero.img,
             hero.weapon,
-            index + 1
+            index + 1,
+            myPlayer === index ? false : true
         ));
     });
 }
+
 const refreshRender = () => {
     playersDOM.forEach(item => {
         $arena.removeChild(item);
@@ -178,9 +188,9 @@ const refreshRender = () => {
     players = [];
     playersDOM = [];
 
-    generatePlayers(JSON.parse(localStorage.getItem('gameMod')));
+    generatePlayers(JSON.parse(localStorage.getItem('gameMod')), 1);
     renderPlayers();
-    $randomBtn.disabled = false;
+    $fightBtn.disabled = false;
 
     if ($arena.querySelector(".winTitle")) {
         const $winTitle = $arena.querySelector(".winTitle")
@@ -188,11 +198,20 @@ const refreshRender = () => {
     }
 
 }
+
 const renderPlayers = () => {
     if (players.length !== 2) return;
 
-    players.forEach((item, index) => {
-        playersDOM.push(createPlayer(item));
+    players.forEach((player, index) => {
+        console.log(player)
+        playersDOM.push(createPlayer(player));
+        
+        if (player.isEnemy) {
+            enemyPlayer = player;
+        } else {
+            myPlayer = player;
+        }
+        
         $arena.appendChild(playersDOM[index]);
     })
 }
@@ -201,20 +220,18 @@ function playerWin(name) {
     const $winTitle = createEl('div', 'winTitle');
     $winTitle.innerText = name + ' win!';
 
-    changeAudio({ src: '19' });
+    changeAudio({ src: '19'});
 
     return $winTitle;
 }
-
-
 
 function createAudio(...attr) {
     window.$audio = createEl('audio');
 
     const attributes = {
         autoplay: "autoplay",
-        loop: "loop",
-        preload: 'auto'
+        // loop: "loop",
+        // preload: 'auto'
     }
 
     Object.assign(attributes, ...attr);
@@ -223,7 +240,6 @@ function createAudio(...attr) {
         $audio.setAttribute(`${key}`, `${attributes[key]}`)
         if (key === 'src') {
             $audio.setAttribute('src', `./assets/audio/${attributes[key]}.mp3`);
-
         }
     }
 
@@ -240,37 +256,19 @@ function changeAudio(...attr) {
 // ----------------------------
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
-        // createAudio({ src: randomInteger(1, 3), allow: "autoplay 'src'" });
+        createAudio({ src: randomInteger(1, 3), allow: "autoplay 'src'", loop: "loop" });
     });
-    generatePlayers(JSON.parse(localStorage.getItem('gameMod')));
+    generatePlayers(JSON.parse(localStorage.getItem('gameMod')), randomInteger(0, 1));
     renderPlayers();
-
-
-    // listeners
-    $randomBtn.addEventListener('click', () => {
-        const whoseMove = Math.floor(Math.random() * 2);
-        Number(whoseMove) === 0 ? (
-            players[0].changeHP(),
-            players[0].renderHP()
-        )
-            : (
-                players[1].changeHP(),
-                players[1].renderHP()
-            )
-
-
-        players.forEach((player, index) => {
-            if (player.hp <= 0) {
-                $randomBtn.disabled = true;
-                const winPlayer = players.filter(item => item.playerNumber !== player.playerNumber)[0].name;
-                $arena.appendChild(playerWin(winPlayer));
-
-                createReloadButton();
-
-            }
-        })
+    
+    console.log('myPlayer', {
+        ...myPlayer
+    })
+    console.log('enemyPlayer', {
+        ...enemyPlayer
     })
 
+    // listeners
     $switchMode.addEventListener("change", function () {
         if (localStorage.getItem('gameMod')) {
             localStorage.removeItem("gameMod");
@@ -278,14 +276,81 @@ document.addEventListener('DOMContentLoaded', function () {
 
         localStorage.setItem("gameMod", this.checked);
         refreshRender();
-        changeAudio({ src: randomInteger(1, 3) });
+        changeAudio({ src: randomInteger(1, 3), loop: "loop" });
     });
 
     $formFight.addEventListener('submit', (event) => {
         event.preventDefault();
+        const $btnSubmit = $formFight.querySelector("button[type='submit']")
 
-        // const enemy = enemyAttack();
-        // console.log( enemy )
+        const myPlayerRaundStep = {};
+
+        for (const item of $formFight) {
+
+            if (item.checked && item.name === 'hit') {
+                myPlayerRaundStep.value = randomInteger(0, HIT[item.value]);
+                myPlayerRaundStep.hit = item.value;
+            }
+
+            if (item.checked && item.name === 'defence') {
+                myPlayerRaundStep.defence = item.value
+            }
+
+            item.checked = false;
+        }
+
+
+        const enemyPlayerRaundStep = enemyPlayer.enemyAttack();
+
+  
+        console.log('myPlayer', {
+            ...myPlayerRaundStep
+        })
+        console.log('enemyPlayer', {
+            ...enemyPlayerRaundStep
+        })
+
+        console.log($btnSubmit);
+        
+        if (myPlayerRaundStep.hit !== enemyPlayerRaundStep.defence) {
+            enemyPlayer.changeHP(myPlayerRaundStep.value);
+        } 
+        if (enemyPlayerRaundStep.hit !== myPlayerRaundStep.defence) {
+            myPlayer.changeHP(enemyPlayerRaundStep.value);
+        }
+
+
+        players.forEach((player, index) => {
+            player.renderHP();
+            if (player.hp <= 0) {
+                $btnSubmit.disabled = true;
+                
+                const winPlayer = players.filter(item => item.playerNumber !== player.playerNumber)[0].name;
+                
+                $arena.appendChild(playerWin(winPlayer));
+                $arena.removeChild($formFight);
+                createReloadButton();
+
+            }
+        })
+        //     const enemyPlayerAttackStats = player.enemyAttack();
+    
+        //     if (player.isEnemy()) {
+        //         players[0].changeHP(enemyPlayerAttackStats.value);
+        //     }
+        //     console.log(myPlayerAttackStats, 'myPlayerAttackStats');
+        //     console.log(enemyPlayerAttackStats, 'enemyPlayerAttackStats');
+        //     player.renderHP();
+    
+            // if (player.hp <= 0) {
+            //     $fightBtn.disabled = true;
+            //     const winPlayer = players.filter(item => item.playerNumber !== player.playerNumber)[0].name;
+            //     $arena.appendChild(playerWin(winPlayer));
+
+            //     createReloadButton();
+
+            // }
+        // })
     })
 });
 

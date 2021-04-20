d7bacc1cd8ac13084b87a5adbbb7562ad5ebfb2b
Belkins18 <nikolay.belibov@gmail.com>
2021-04-21 01:09:55 +0300

    hw7

---
+++ src/js/domEls.js
@@ -0,0 +1,5 @@
+export const $root = document.querySelector(".root");
+export const $arena = document.querySelector(".arenas");
+export const $switchMode = document.getElementById("checkbox");
+export const $formFight = document.querySelector(".control");
+export const $chat = document.querySelector(".chat");
